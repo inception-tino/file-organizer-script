49d57d164459011a806cc26172f41b3bb6e20188
inception-tino <x369sage@outlook.com>
2024-07-25 01:57:53 +0000

    Implemented file type detection

---
+++ scripts/file_type_detection.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Array to hold file types
+declare -A file_types
+
+# Function to detect file types
+detect_file_types() {
+    echo -e "\e[34mStarting file type detection...\e[0m"  # Blue text
+    for file in $(ls -p | grep -v /); do
+        if [ -f "$file" ]; then
+            extension="${file##*.}"
+            file_types["$file"]="$extension"
+        fi
+    done
+    echo -e "\e[34mFinished file type detection.\e[0m"  # Blue text
+}
+
